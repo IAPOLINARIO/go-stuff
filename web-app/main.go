5c9322b4a14e18db67f459b9aa46face8cce733d
Ismael Apolinario <ismaelapolinario@hotmail.com>
2021-02-05 21:00:58 +0100

    Adding faq page

--- web-app/main.go
+++ web-app/main.go
@@ -17,10 +17,24 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@ismael.com\"> support@ismael.com</a>")
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "This is the faq section")
+}
+
+func Handle404() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		fmt.Fprint(w, "Sorry your page could not be found")
+	})
+}
+
 func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+	r.NotFoundHandler = Handle404()
 	http.ListenAndServe(":3000", r)
 }
